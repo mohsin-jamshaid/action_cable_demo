62c30b1dda8df4a5b232930707c7a81e3bd9ab02
mohsin-jamshaid <mohsin.jamshaid007@gmail.com>
2020-01-04 15:26:46 +0500

    Warden implemented for passing current user to channel through cookies, Action Cable completed

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root 'rooms#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  mount ActionCable.server => '/cable'
 end
